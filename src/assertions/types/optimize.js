9f0476cd1ec6823920efdc3ae319f9e93734a0c8
Adam Eilers <adam.eilers@gmail.com>
2018-07-11 19:08:03 -0700

    removing nested async functions in optimized assertions

--- src/assertions/types/optimize.js
+++ src/assertions/types/optimize.js
@@ -4,16 +4,16 @@ import { isInteger } from './primitive'
  * optimized assertions
  */
 export const OPTIMIZED = Symbol('cache of thunked methods to validate a value')
-export async function assertOptimized (value, schema, optimized = []) {
+export function assertOptimized (value, schema, optimized = []) {
   if (schema === false) throw new Error('\'false\' Schema invalidates all values')
-  return Promise.all(optimized.map(fn => fn(value, schema)))
+  for (let fn of optimized) fn(value, schema)
 }
 
 export function assertSizeMax (size, key) {
   if (!(isInteger(size) && size > 0)) {
     throw new TypeError(`#${key}: keyword must be a positive integer`)
   }
-  return async (results, ref) => {
+  return (results, ref) => {
     if (results.length > ref[key]) throw new Error(`#${key}: value maximum exceeded`)
   }
 }
@@ -22,7 +22,7 @@ export function assertSizeMin (size, key) {
   if (!(isInteger(size) && size > 0)) {
     throw new TypeError(`#${key}: keyword must be a positive integer`)
   }
-  return async (results, ref) => {
+  return (results, ref) => {
     if (results.length < ref[key]) throw new Error(`#${key}: value minimum not met`)
   }
 }
